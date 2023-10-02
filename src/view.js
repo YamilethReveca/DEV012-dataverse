e3aa3c3dd03d6aa3c8c8baefaf7025766e591afe
MrMidnight02 <itzel.garce@gmail.com>
2023-10-02 16:14:44 -0600

    cambios en view.js para agregar li

--- src/view.js
+++ src/view.js
@@ -1,6 +1,38 @@
 export const renderItems = (data) => {
-  console.log(data)
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
+
+  const ul = document.createElement("ul");
+
+  for (let i = 0; i < data.length; i++) {
+    let objeto = data[i];
+
+    let li = document.createElement("li");
+  
+   li.innerHTML = `
+   <img src="${objeto.imgUrl}" alt="Imagen de ${objeto.name}">
+   <dl>
+     <dt>Nombre:</dt>
+     <dd itemprop="name">${objeto.name}</dd>
+     <dt>Corta descripción:</dt>
+     <dd itemprop="shortDescription">${objeto.shortDescription}</dd>
+     <dt>Descripción:</dt>
+     <dd itemprop="description">${objeto.description}</dd>
+     <dt>Campo principal</dt>
+     <dd itemprop="mainField">${objeto.mainField}</dd>
+     <dt>Habilidades:</dt>
+     <dd itemprop="habilities">${objeto.habilities}</dd>
+     <dt>Peso:</dt>
+     <dd itemprop="wight">${objeto.wight}</dd>
+   </dl>
+ `;
+     
+ ul.appendChild(li);
+}
+
+  const pokemon = document.querySelector("ul");
+
+  pokemon.appendChild(li);
+
   return 'example';
 };
+
 
