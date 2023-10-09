03e04ddd64e926a809371ffefb466f132e34e0fd
Yamileth Reveca <yamilethcaldera@gmail.com>
2023-10-09 16:47:16 -0300

    agregando funcionalidades del filtro, ordenamiento

--- src/main.js
+++ src/main.js
@@ -1,20 +1,13 @@
-
-import { filterData } from './dataFunctions.js';
-import { sortData } from './dataFunctions.js';
+import { filterData , sortData } from './dataFunctions.js';
 import { renderItems } from './view.js';
-
-
-
 import data from './data/dataset.js';
 
 
 //console.log(example, renderItems(data), data);
 
 
-
-
 // accedemos al dom del elemento padre, la sección por su id
-// creamos el 
+
 
 let section= document.querySelector("#root");
 
@@ -25,35 +18,26 @@ section.appendChild(renderItems(data));
 
 // accedemos al dom con el querySelector a traves de su atributo.
 const clearButton = document.querySelector('[data-testid="button-clear"]');
-const selectFilter = document.querySelector('[data-testid="select-filter"]')
-const selectSort  = document.querySelector('[data-testid="select-sort"]')
-const inputRadios = document.querySelectorAll('input[type="radio"][name="sort-order"]');
-
-// input tipo radio
-const radioAsc = document.querySelector('input[type="radio"][value="asc"]');
-const radioDesc = document.querySelector('input[type="radio"][value="desc"]');
-
+const selectFilter = document.querySelector('[data-testid="select-filter"]');
+const selectSort = document.querySelector('[data-testid="select-sort"]')
 
 
 
 //el evento addEventListener para el boton, limpia el filtro,ordenamiento y los input radios.
 clearButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    selectFilter.selectedIndex = -1;
+    event.preventDefault();// // Evita el comportamiento predeterminado del botón
+    selectFilter.selectedIndex = -1; // Establece la selección del select en ninguna opción (-1)
     selectSort.selectedIndex = -1;
-    inputRadios.forEach(function (radio) {
-        radio.checked = false;
-    });
-
+    
     section.innerHTML="";
 
-   section.appendChild(renderItems(data));
+    section.appendChild(renderItems(data));
 
 });
 
 
 // funcionamiento del filtro. 
-selectFilter.addEventListener("change", function () {
+  selectFilter.addEventListener("change", function () {
     const selectedValue = selectFilter.value;
 
     const filteredData = filterData(data, "mainField", selectedValue);
@@ -61,41 +45,31 @@ selectFilter.addEventListener("change", function () {
     section.innerHTML="";
     section.appendChild(renderItems(filteredData));
 
+    console.log("data" , data) 
+    console.log("seleccion", selectedValue)
+    console.log("filtrado", filteredData)
 
 
+     //ordenamiento
 
-console.log("data" , data) 
-console.log("seleccion", selectedValue)
-console.log(filteredData)
+   selectSort.addEventListener("change", function () {
+      const selectedValue = selectSort.value;
+      const arraySort= selectedValue.split("-");
+      const sortedData = sortData(filteredData, arraySort[0], arraySort[1]);
+        
+      
+        // Actualiza la página visualmente con los datos ordenados
+        section.innerHTML = "";
+        section.appendChild(renderItems(sortedData));
 
-});
+       
 
-
-
-// ordenamiento 
-
-
-
-
-selectSort.addEventListener("change", function () {
-    const selectedValue = selectSort.value;
-
-    const sortOrder = radioAsc.checked ? "asc" : radioDesc.checked ? "desc" : "";
-
-    const sortedData = sortData(data, selectedValue, sortOrder);
-
-    section.innerHTML="";
-    section.appendChild(renderItems(sortedData));
-    
-
-   
-
-
-console.log("data" , data) 
-console.log("seleccion", selectedValue)
-console.log(sortOrder)
-
-});
+        console.log("data ordenamiento" , data) 
+        console.log("seleccion ordenamiento", selectedValue)
+        console.log("data asc o desc", sortedData)
+      
+    });
+  });
 
 
 
