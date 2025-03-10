a85e71d172bd369f715449d5abe25294eacc4245
yamileth caldera <yamilethcaldera@gmail.com>
2025-03-09 22:28:09 -0300

    Updates

--- view.js
+++ view.js
@@ -2,9 +2,9 @@ export const renderItems = (data) => {
 
   const ul = document.createElement("ul");// creamos el ul
 
-  data.forEach(pokemon => { // ciclo for para recorrer los arreglos 
+  data.forEach(pokemon => { // ciclo for para recorrer los arreglos
 
-  
+
     const li= document.createElement("li");  // creamos en li
     li.classList.add("tarjetas");
     const itemscope = document.createAttribute("itemscope");
@@ -12,25 +12,25 @@ export const renderItems = (data) => {
     itemtype.value="tarjetaPokemon";
     li.setAttributeNode(itemscope);
     li.setAttributeNode(itemtype);
-  
+
     li.innerHTML= "";  // inicializamos el li vacio.
     let content =`<dl><img class="imgTarjeta" src="${pokemon.imageUrl}" alt="${pokemon.id}">
-<dt><strong>Nombre:</stron></dt><dd itemprop="name"><stron>${pokemon.name}</strong></dd>
-<dt><strong>Corta descripción:</strong></dt><dd itemprop="shortDescription">${pokemon.shortDescription}</dd>
+<dt><strong></stron></dt><dd itemprop="name"><stron>${pokemon.name}</strong></dd>
+<dt><strong>Corta Descripción</strong></dt><dd itemprop="shortDescription">${pokemon.shortDescription}</dd>
 <dt><strong>Descripción:</strong></dt><dd class="estilo" itemprop="description">${pokemon.description}</dd>`;
 
 
 
     const facts = pokemon.facts;
     if (facts) {
-      content += `<dt><strong>Campo principal:</strong></dt><dd itemprop="mainField">${facts.mainField}</dd>
-  <dt><strong>Habilidades:</strong></dt><dd itemprop="pokemonHabilities">${facts.pokemonHabilities}</dd>
-  <dt><strong>Peso:</strong></dt><dd itemprop="pokemonWeight">${facts.pokemonWeight} Kg</dd></dl>`;
+      content += `<dt><strong>Campo Principal</strong></dt><dd itemprop="mainField">${facts.mainField}</dd>
+  <dt><strong>Habilidades</strong></dt><dd itemprop="pokemonHabilities">${facts.pokemonHabilities}</dd>
+  <dt><strong>Peso</strong></dt><dd itemprop="pokemonWeight">${facts.pokemonWeight} Kg</dd></dl>`;
     }
 
     li.innerHTML=content;
 
-    ul.appendChild(li)                       
+    ul.appendChild(li)
   });
 
   return ul;
